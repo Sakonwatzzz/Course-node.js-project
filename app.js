d4d09b2ce60033ed9bafb3f66120f3de578fde3e
Sakonwatzzz <149657557+Sakonwatzzz@users.noreply.github.com>
2024-10-17 15:58:31 +0700

    add free templates
    add free templates from web site One love page : https://onepagelove.com/templates

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ import express from 'express';
 import chalk from 'chalk';
 import createDebug from 'debug'
 import morgan from 'morgan';
+import path  from 'path';
 
-
-const debug = createDebug('app')
+const debug = createDebug('app');
 const app = express();
 const port = process.env.PORT || 3000;  // ลองใช้พอร์ต 3001 หาก 3000 ถูกใช้งาน
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(process.cwd(), "public")));
 
     app.get('/', (req, res) => {
     res.send('Welcome ');
